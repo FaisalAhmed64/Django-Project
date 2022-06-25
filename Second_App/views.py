29aea7e491616c2329e8c07f027543c98425f9e8
Md Faisal Ahmed <68557284+FaisalAhmed64@users.noreply.github.com>
2022-06-26 02:17:03 +0600

    creating template and connect file

--- Second_App/views.py
+++ Second_App/views.py
@@ -1,7 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict={'insert_me': "I am from views file"}
+    return render(request, 'second_app/index.html', context=my_dict)
